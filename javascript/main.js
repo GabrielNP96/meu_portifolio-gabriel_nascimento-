803bd8f057888b1106489361083731710a12efbb
GabrielNP96 <gabrielnp1996@gmail.com>
2023-11-04 11:45:59 -0300

    add projetos

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+const listaDeTarefas = {
+    'nome' : 'Lista de Tarefas',
+    'link' : 'https://gabrielnp96.github.io/lista_de_tarefas/',
+    'descricao' : 'Uma lista de tarefas dinâmica, feita com javascript, HTML e Css, ela utiliza do local storage do navegador, para armazenar os dados, então mesmo que você feche o navegador os dados vão continuar lá.',
+    'imagem' : './image/Captura de tela_2023-11-02_12-33-13.png'
+}
+
+const projects = document.querySelector('.projects-area')
+
+projects.innerHTML = `<div class='lista-de-tarefas'>
+                        <h3>${listaDeTarefas.nome}</h3>
+                        <a href="${listaDeTarefas.link}" target ='_blank'><img src='${listaDeTarefas.imagem}' alt='imagem do projeto'></a>
+                        <p>${listaDeTarefas.descricao}</p>
+                    </div>`
