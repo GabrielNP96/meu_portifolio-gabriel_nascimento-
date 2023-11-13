05dd25a8e768129331277a9ef46bc03103dbbb02
GabrielNP96 <gabrielnp1996@gmail.com>
2023-11-13 04:59:29 -0300

    add responsividade, add js

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,39 @@
-const listaDeTarefas = {
-    'nome' : 'Lista de Tarefas',
-    'link' : 'https://gabrielnp96.github.io/lista_de_tarefas/',
-    'descricao' : 'Uma lista de tarefas dinâmica, feita com javascript, HTML e Css, ela utiliza do local storage do navegador, para armazenar os dados, então mesmo que você feche o navegador os dados vão continuar lá.',
-    'imagem' : './image/Captura de tela_2023-11-02_12-33-13.png'
+const projectsData = [
+    {
+        name: 'Lista de Tarefas',
+        link: 'https://gabrielnp96.github.io/lista_de_tarefas/',
+        description: 'Uma lista de tarefas dinâmica, feita com javascript, HTML e Css, ela utiliza do local storage do navegador, para armazenar os dados, então mesmo que você feche o navegador os dados vão continuar lá.',
+        image: './image/Captura de tela_2023-11-02_12-33-13.png'
+    },
+    {
+        name: 'Calculadora IMC',
+        link: 'https://gabrielnp96.github.io/Calculadora_IMC/',
+        description: 'Uma calculadora de IMC',
+        image: './image/Captura de tela_2023-11-13_04-57-37.png'
+    },
+];
+
+function createProjectElement(project) {
+    const projectElement = document.createElement('div');
+    projectElement.classList.add('my-projects');
+
+    projectElement.innerHTML = `
+        <h3>${project.name}</h3>
+        <a href="${project.link}" target ='_blank'><img src='${project.image}' alt='imagem do projeto'></a>
+        <p>${project.description}</p>
+        <a class="project-link" href="${project.link}" target="_blank">Ver Projeto</a>
+    `;
+
+    return projectElement;
 }
 
-const projects = document.querySelector('.projects-area')
+function renderProjects() {
+    const projectsArea = document.querySelector('.projects-area');
 
-function projectsOnHtml(projects) {
-    projects.innerHTML = `<div class='my-projects'>
-                        <h3>${listaDeTarefas.nome}</h3>
-                        <a href="${listaDeTarefas.link}" target ='_blank'><img src='${listaDeTarefas.imagem}' alt='imagem do projeto'></a>
-                        <p>${listaDeTarefas.descricao}</p>
-                    </div>`
+    projectsData.forEach((project) => {
+        const projectElement = createProjectElement(project);
+        projectsArea.appendChild(projectElement);
+    });
 }
 
-projectsOnHtml(projects)
+window.onload = renderProjects;
